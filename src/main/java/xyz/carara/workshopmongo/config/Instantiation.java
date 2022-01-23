c206daaf661d1bff749a7758d220e20b9de813fd
Pedro Carara <pedro.carara@meta.com.br>
2022-01-23 15:46:09 -0300

    Adicionado AuthorDTO para projetar dados do author no post

--- src/main/java/xyz/carara/workshopmongo/config/Instantiation.java
+++ src/main/java/xyz/carara/workshopmongo/config/Instantiation.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import xyz.carara.workshopmongo.domain.Post;
 import xyz.carara.workshopmongo.domain.User;
+import xyz.carara.workshopmongo.dto.AuthorDTO;
 import xyz.carara.workshopmongo.repository.PostRepository;
 import xyz.carara.workshopmongo.repository.UserRepository;
 
@@ -32,10 +33,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/01/2022"),"Partiu viajar", "Vou  viajar para São Paulo, abraços a todos!", maria);
-        Post post2 = new Post(null, sdf.parse("23/01/2022"),"Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/01/2022"),"Partiu viajar", "Vou  viajar para São Paulo, abraços a todos!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/01/2022"),"Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
