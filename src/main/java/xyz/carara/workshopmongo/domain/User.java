b5e0568efd5350c330608bad7a455411eb132622
Pedro Carara <pedro.carara@meta.com.br>
2022-01-23 16:00:28 -0300

    Adicionado referencia de posts nos usuários

--- src/main/java/xyz/carara/workshopmongo/domain/User.java
+++ src/main/java/xyz/carara/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package xyz.carara.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection="user")
 public class User implements Serializable {
@@ -12,6 +15,8 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -46,6 +51,13 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 
     @Override
     public int hashCode() {
