b682d4da4397e1fcaf7ac18030f6bcbad4c38a08
Pedro Carara <pedro.carara@meta.com.br>
2022-01-23 10:10:55 -0300

    Adicionado endpoint POST para inserção de usuários

--- src/main/java/xyz/carara/workshopmongo/resources/UserResource.java
+++ src/main/java/xyz/carara/workshopmongo/resources/UserResource.java
@@ -2,14 +2,13 @@ package xyz.carara.workshopmongo.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import xyz.carara.workshopmongo.domain.User;
 import xyz.carara.workshopmongo.dto.UserDTO;
 import xyz.carara.workshopmongo.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +29,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO user){
+        User newUser = service.insert(service.fromDTO(user));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(("/{id}")).buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/xyz/carara/workshopmongo/services/UserService.java
+++ src/main/java/xyz/carara/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package xyz.carara.workshopmongo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xyz.carara.workshopmongo.domain.User;
+import xyz.carara.workshopmongo.dto.UserDTO;
 import xyz.carara.workshopmongo.repository.UserRepository;
 import xyz.carara.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User user){
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO user){
+        return new User(user.getId(), user.getName(), user.getName());
     }
 
 }
